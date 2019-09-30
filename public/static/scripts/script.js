d7ff21123d79b54cb23923d56cc02d6fc4ac0841
Kevin Streiter <kevin.streiter@gmail.com>
2019-09-30 12:02:32 +0200

    Circle in the middle of upper border

--- public/static/scripts/script.js
+++ public/static/scripts/script.js
@@ -2,7 +2,9 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var d3 = require("./modules/d3.js");
 window.onload = function () {
+    var g;
     var rect;
+    var circle;
     var deltaX;
     var deltaY;
     var svg = d3.select("#graph")
@@ -13,7 +15,9 @@ window.onload = function () {
         .on("drag", dragmove);
     function mousedown() {
         var event = d3.mouse(this);
-        rect = svg.append("rect")
+        g = svg.append("g")
+            .call(drag);
+        rect = g.append("rect")
             .attr("x", event[0])
             .attr("y", event[1])
             .attr("rx", 20)
@@ -23,13 +27,29 @@ window.onload = function () {
             .attr("stroke", "#7b9eb4")
             .attr("stroke-width", 4)
             .attr("fill", "#aaa9ad")
-            .call(drag);
+            .call(drag)
+            .on("mouseover", function () {
+            d3.select(this)
+                .style("cursor", "pointer");
+        })
+            .on("mouseout", function () {
+            d3.select(this)
+                .style("cursor", "default");
+        });
+        circle = g.append("circle")
+            .attr("cx", (+rect.attr("x") + (+rect.attr("width") / 2)))
+            .attr("cy", +rect.attr("y"))
+            .attr("r", 5)
+            .attr("fill", "grey");
         svg.on("mousemove", mousemove);
     }
     function mousemove() {
         var event = d3.mouse(this);
         rect.attr("width", Math.max(0, event[0] - +rect.attr("x")))
             .attr("height", Math.max(0, event[1] - +rect.attr("y")));
+        circle
+            .attr("cx", (+rect.attr("x") + (+rect.attr("width") / 2)))
+            .attr("cy", +rect.attr("y"));
     }
     function dragstart() {
         var current = d3.select(this);
@@ -37,11 +57,13 @@ window.onload = function () {
         deltaY = current.attr("y") - d3.event.y;
     }
     function dragmove() {
-        if (d3.event.sourceEvent.altKey) {
-            d3.select(this)
-                .attr("x", d3.event.x + deltaX)
-                .attr("y", d3.event.y + deltaY);
-        }
+        d3.select(this)
+            .attr("x", d3.event.x + deltaX)
+            .attr("y", d3.event.y + deltaY);
+        var circle = d3.select(this.parentNode).select("circle");
+        circle
+            .attr("cx", (d3.event.x + deltaX) + (+d3.select(this).attr("width") / 2))
+            .attr("cy", (d3.event.y + deltaY));
     }
     function mouseup() {
         svg.on("mousemove", null);
