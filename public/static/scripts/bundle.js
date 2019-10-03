f2f868d7cc87a1a646ce9f29818e9acfc7a8cc17
Kevin Streiter <kevin.streiter@gmail.com>
2019-10-03 18:05:22 +0200

    Line move with rect (only parent) -> will be implemented soon

--- public/static/scripts/bundle.js
+++ public/static/scripts/bundle.js
@@ -18496,7 +18496,6 @@ window.onload = function () {
             .attr("class", "circleRight")
             .attr("fill", "grey");
         d3.selectAll("circle")
-            .raise()
             .on('mouseover', function () {
             d3.select(this)
                 .attr("r", 10);
@@ -18510,6 +18509,7 @@ window.onload = function () {
         svg.on("mousemove", mousemove);
     }
     function mousemove() {
+        var parent = d3.select(this.parentNode);
         var event = d3.mouse(this);
         rect.attr("width", Math.max(0, event[0] - +rect.attr("x")))
             .attr("height", Math.max(0, event[1] - +rect.attr("y")));
@@ -18542,18 +18542,30 @@ window.onload = function () {
                 .attr("x", d3.event.x + deltaX)
                 .attr("y", d3.event.y + deltaY);
             var parent_1 = d3.select(this.parentNode);
-            parent_1.select("circle.circleTop")
+            var top_1 = parent_1.select("circle.circleTop")
                 .attr("cx", (d3.event.x + deltaX) + (+d3.select(this).attr("width") / 2))
                 .attr("cy", (d3.event.y + deltaY));
-            parent_1.select("circle.circleBottom")
+            var bottom = parent_1.select("circle.circleBottom")
                 .attr("cx", (d3.event.x + deltaX) + (+d3.select(this).attr("width") / 2))
                 .attr("cy", (d3.event.y + deltaY) + +d3.select(this).attr("height"));
-            parent_1.select("circle.circleLeft")
+            var left = parent_1.select("circle.circleLeft")
                 .attr("cx", (d3.event.x + deltaX))
                 .attr("cy", (d3.event.y + deltaY) + (+d3.select(this).attr("height") / 2));
-            parent_1.select("circle.circleRight")
+            var right = parent_1.select("circle.circleRight")
                 .attr("cx", (d3.event.x + deltaX) + +d3.select(this).attr("width"))
                 .attr("cy", (d3.event.y + deltaY) + (+d3.select(this).attr("height") / 2));
+            parent_1.selectAll("line.circleTop")
+                .attr("x1", (d3.event.x + deltaX) + (+d3.select(this).attr("width") / 2))
+                .attr("y1", d3.event.y + deltaY);
+            parent_1.selectAll("line.circleBottom")
+                .attr("x1", (d3.event.x + deltaX) + (+d3.select(this).attr("width") / 2))
+                .attr("y1", (d3.event.y + deltaY) + +d3.select(this).attr("height"));
+            parent_1.selectAll("line.circleLeft")
+                .attr("x1", (d3.event.x + deltaX))
+                .attr("y1", (d3.event.y + deltaY) + (+d3.select(this).attr("height") / 2));
+            parent_1.selectAll("line.circleRight")
+                .attr("x1", (d3.event.x + deltaX) + +d3.select(this).attr("width"))
+                .attr("y1", (d3.event.y + deltaY) + (+d3.select(this).attr("height") / 2));
         }
     }
     function mouseup() {
@@ -18561,15 +18573,17 @@ window.onload = function () {
     }
     function drawLine() {
         var current = d3.select(this);
+        var parent = d3.select(this.parentNode);
         var cx = current.attr("cx");
         var cy = current.attr("cy");
-        line = svg.append("line");
+        line = parent.append("line");
         line.attr("x1", cx)
             .attr("y1", cy)
             .attr("x2", cx)
             .attr("y2", cy)
             .attr("stroke", "grey")
-            .attr("stroke-width", 3);
+            .attr("stroke-width", 3)
+            .attr("class", current.attr("class"));
         svg.on("mousemove", moveLine);
     }
     function removeLine() {
@@ -18599,10 +18613,23 @@ window.onload = function () {
     }
     function combineRect() {
         var current = d3.select(this);
-        line
-            .attr("x2", current.attr("cx"))
-            .attr("y2", current.attr("cy"));
-        resetListeners();
+        var parent = d3.select(this.parentNode);
+        var x1 = line.attr("x1");
+        var y1 = line.attr("y1");
+        var sameRect = false;
+        parent.selectAll("circle").each(function () {
+            var cx = d3.select(this).attr("cx");
+            var cy = d3.select(this).attr("cy");
+            if (x1 == cx && y1 == cy) {
+                sameRect = true;
+            }
+        });
+        if (!sameRect) {
+            line
+                .attr("x2", current.attr("cx"))
+                .attr("y2", current.attr("cy"));
+            resetListeners();
+        }
     }
 };
 
